528aedee7ef61a3daa764126d97ed524234e1099
zhouchenhua <noatnu@foxmail.com>
2022-09-29 17:18:25 +0800

    fix

---
+++ v2/type_transform.py
@@ -0,0 +1,25 @@
+
+
+var_a = "123"
+
+var_int = int(var_a)
+
+var_string = str(var_int)
+
+print(type(var_a),var_a)
+
+print(type(var_int),var_int)
+
+print(type(var_string),var_string)
+
+var_float = 3.14
+
+var_double = 32.23632
+
+
+
+
+
+
+
+
